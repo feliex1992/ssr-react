ade49ad33e25692c84113b648117b99c01b0a6bf
feliex1992 <jafar.pahrudin@gmail.com>
2020-06-17 18:59:37 +0700

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,6 @@
-const path = require('path');
-
 module.exports = {
   entry: './src/index.js',
   module: {
-    // rules: [
-    //   {
-    //     test: /\.js$/,
-    //     use: [
-    //       {
-    //         loader: path.resolve('path/to/loader.js'),
-    //         options: {'dev'}
-    //       }
-    //     ]
-    //   }
-    // ]
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
